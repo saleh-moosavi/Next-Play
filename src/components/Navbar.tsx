66c26c5aabd670b868ce21e0974c64403f1f1d26
Saleh Moosavi <salehmoosavi77@gmail.com>
2024-12-04 14:12:50 +0330

    feat:add Sidebar section for mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,12 @@ import { CiSearch } from "react-icons/ci";
 import { FaGamepad } from "react-icons/fa";
 import { IoMdMoon } from "react-icons/io";
 import { PiList } from "react-icons/pi";
+import Sidebar from "./Sidebar";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
   return (
     <div className="text-white flex justify-between items-center p-5 lg:px-0 lg:max-w-5xl lg:mx-auto">
       <ul className="hidden sm:flex sm:gap-x-5 sm:items-center *:text-sm font-semibold *:text-gray-200">
@@ -16,7 +20,13 @@ export default function Navbar() {
         <li>تماس با ما</li>
         <li>وبلاگ</li>
       </ul>
-      <PiList className="sm:hidden size-6 cursor-pointer hover:scale-110 transition-all duration-500" />
+      <div className="sm:hidden flex gap-x-5 *:cursor-pointer">
+        <PiList
+          className="size-6 hover:scale-110 transition-all duration-500"
+          onClick={() => setSidebar(true)}
+        />
+        <FaGamepad className="w-7 h-7 text-purple-500" />
+      </div>
       <article className="flex gap-x-5 items-center">
         <CiSearch />
         <IoMdMoon />
@@ -24,6 +34,14 @@ export default function Navbar() {
           ثبت نام
         </button>
       </article>
+
+      {/* sidebar */}
+      <div
+        className={`sm:hidden fixed top-0 bottom-0 right-0 z-50 bg-black/30 backdrop-blur-sm w-full h-full transition-all duration-500 flex
+          ${sidebar ? "opacity-100 visible" : "opacity-0 invisible"}`}
+      >
+        <Sidebar setSidebar={setSidebar} sidebar={sidebar} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,22 @@
+import { IoMdClose } from "react-icons/io";
+
+export default function Sidebar({ setSidebar, sidebar }: any) {
+  return (
+    <ul
+      className={`flex flex-col gap-5 p-5 items-start rounded-2xl m-auto w-1/2 h-1/2 bg-purple-500/80 *:text-sm font-semibold transition-all duration-300 *:text-white *:cursor-pointer *:transition-all *:duration-500 
+        ${sidebar ? "translate-y-0" : "-translate-y-full"}`}
+    >
+      <li>
+        <IoMdClose
+          className="w-7 h-7 text-white hover:text-rose-200 hover:rotate-180 transition-all duration-500"
+          onClick={() => setSidebar(false)}
+        />
+      </li>
+      <li className="hover:-translate-x-2">خانه</li>
+      <li className="hover:-translate-x-2">فروشگاه</li>
+      <li className="hover:-translate-x-2">درباره ما</li>
+      <li className="hover:-translate-x-2">تماس با ما</li>
+      <li className="hover:-translate-x-2">وبلاگ</li>
+    </ul>
+  );
+}
