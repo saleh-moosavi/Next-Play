e3e2f779336b84b578082b084d584aec45842885
Saleh Moosavi <salehmoosavi77@gmail.com>
2024-12-04 12:29:03 +0330

    style:Navbar section mobile responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import { CiSearch } from "react-icons/ci";
 import { FaGamepad } from "react-icons/fa";
 import { IoMdMoon } from "react-icons/io";
+import { PiList } from "react-icons/pi";
 
 export default function Navbar() {
   return (
-    <div className="text-white flex justify-between p-5 lg:px-0 items-center lg:max-w-5xl lg:mx-auto">
-      <ul className="flex gap-x-5 items-center *:text-sm font-semibold *:text-gray-200">
+    <div className="text-white flex justify-between items-center p-5 lg:px-0 lg:max-w-5xl lg:mx-auto">
+      <ul className="hidden sm:flex sm:gap-x-5 sm:items-center *:text-sm font-semibold *:text-gray-200">
         <li>
           <FaGamepad className="w-7 h-7 text-purple-500" />
         </li>
@@ -15,6 +16,7 @@ export default function Navbar() {
         <li>تماس با ما</li>
         <li>وبلاگ</li>
       </ul>
+      <PiList className="sm:hidden size-6 cursor-pointer hover:scale-110 transition-all duration-500" />
       <article className="flex gap-x-5 items-center">
         <CiSearch />
         <IoMdMoon />
