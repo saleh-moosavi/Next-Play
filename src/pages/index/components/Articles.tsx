db41db982d661834004ebeb3c18b8499037dd374
Saleh Moosavi <salehmoosavi77@gmail.com>
2024-12-11 15:42:27 +0330

    fix:darkmod calss in Article section

--- src/pages/index/components/Articles.tsx
+++ src/pages/index/components/Articles.tsx
@@ -5,7 +5,7 @@ import articles from "../../../db/articles.json";
 export default function Articles() {
   return (
     <div className="pb-10 px-5 pt-5 lg:px-0">
-      <h3 className="border-b-2 w-fit mx-auto border-white border-gray-900 text-white dark:text-gray-900 pb-2 my-5">
+      <h3 className="border-b-2 w-fit mx-auto border-white dark:border-gray-900 text-white dark:text-gray-900 pb-2 my-5">
         جدیدترین مقالات
       </h3>
       <section className="grid sm:grid-cols-2 gap-5 mt-10">
