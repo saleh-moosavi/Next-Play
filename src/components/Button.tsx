a9973a3fcc5b7176bcc27dc3e807ed4921355fd1
Saleh Moosavi <salehmoosavi77@gmail.com>
2024-12-12 10:57:44 +0330

    style:Review & Button darkmod colors

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button() {
   return (
-    <button className="bg-white/20 rounded-lg text-white px-4 py-2 mt-8 text-xs font-semibold hover:shadow-[0_0_5px_#fff] transition-all duration-300">
+    <button className="bg-white/20 dark:bg-black/15 rounded-lg text-white dark:text-gray-900 px-4 py-2 mt-8 text-xs font-semibold hover:shadow-[0_0_5px_#fff] dark:hover:shadow-[0_0_5px_#000] transition-all duration-300">
       مشاهده همه
     </button>
   );
--- src/pages/index/components/Reviews.tsx
+++ src/pages/index/components/Reviews.tsx
@@ -5,7 +5,7 @@ import Button from "../../../components/Button";
 export default function Reviews() {
   return (
     <div className="pb-10 px-5 pt-5 lg:px-0 text-center">
-      <h3 className="border-b-2 w-fit mx-auto border-white text-white pb-2 my-5">
+      <h3 className="border-b-2 w-fit mx-auto border-white dark:border-gray-900 text-white dark:text-gray-900 pb-2 my-5">
         نقد و بررسی ها
       </h3>
       <section className="grid sm:grid-cols-3 items-center justify-items-center gap-5 mt-10">
