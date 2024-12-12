a5fa295ca34eb5d6170bff8ec0d2f5dba185557b
Saleh Moosavi <salehmoosavi77@gmail.com>
2024-12-12 12:25:18 +0330

    style:Trailers top section darkmod colors

--- src/pages/index/components/TrailerTop.tsx
+++ src/pages/index/components/TrailerTop.tsx
@@ -1,49 +1,46 @@
-import {BiSolidDislike, BiSolidLike} from "react-icons/bi";
-import {CiBookmarkPlus} from "react-icons/ci";
-import {GoKebabHorizontal} from "react-icons/go";
-import {IoIosShareAlt} from "react-icons/io";
+import { BiSolidDislike, BiSolidLike } from "react-icons/bi";
+import { CiBookmarkPlus } from "react-icons/ci";
+import { GoKebabHorizontal } from "react-icons/go";
+import { IoIosShareAlt } from "react-icons/io";
 import VideoPlayer from "../../../components/VideoPlayer";
 
 export default function TrailerTop() {
-    return (
-        <div className="bg-white/5 rounded-2xl overflow-hidden">
-            <div className="w-full mb-3 overflow-hidden rounded-2xl">
-                <VideoPlayer/>
+  return (
+    <div className="bg-white/5 dark:bg-black/10 rounded-2xl overflow-hidden">
+      <div className="w-full mb-3 overflow-hidden rounded-2xl">
+        <VideoPlayer />
+      </div>
+      <div className="text-white dark:text-gray-900 text-right p-5">
+        <h2 className="text-lg font-semibold mb-2">
+          تریلر جذاب بازی Call Of Duty Mobile
+        </h2>
+        <p className="text-xs font-semibold text-gray-300 dark:text-gray-700 leading-6 text-justify">
+          فصل جدید بازی کالاف دیوتی موبایل که همین چند روز پیش و در تاریخ ۱۱ مهر
+          منتشر شد، غافلگیری‌های زیادی را برای طرفداران رو کرد. این فصل جدید که
+          Orbital Raiders نام دارد حال و هوایی علمی-تخیلی به بازی می‌دهد و آن را
+          متنوع‌تر از همیشه نشان می‌دهد.
+        </p>
+        <hr className="my-5 opacity-20 dark:border-gray-900" />
+        <article className="flex justify-between items-center">
+          <section className="text-white flex gap-x-10">
+            <div className="text-xs flex gap-x-2 items-center cursor-pointer dark:text-gray-900 hover:text-stone-300 dark:hover:text-stone-500 transition-all duration-700">
+              <CiBookmarkPlus className="w-5 h-5" />
+              <p className="hidden sm:block">افزودن به لیست علاقه مندی ها</p>
             </div>
-            <div className="text-white text-right p-5">
-                <h2 className="text-lg font-semibold mb-2">
-                    تریلر جذاب بازی Call Of Duty Mobile
-                </h2>
-                <p className="text-xs font-semibold text-gray-300 leading-6 text-justify">
-                    فصل جدید بازی کالاف دیوتی موبایل که همین چند روز پیش و در تاریخ ۱۱ مهر
-                    منتشر شد، غافلگیری‌های زیادی را برای طرفداران رو کرد. این فصل جدید که
-                    Orbital Raiders نام دارد حال و هوایی علمی-تخیلی به بازی می‌دهد و آن را
-                    متنوع‌تر از همیشه نشان می‌دهد.
-                </p>
-                <hr className="my-5 opacity-20"/>
-                <article className="flex justify-between items-center">
-                    <section className="text-white flex gap-x-10">
-                        <div
-                            className="text-xs flex gap-x-2 items-center cursor-pointer hover:text-stone-300 transition-all duration-700">
-                            <CiBookmarkPlus className="w-5 h-5"/>
-                            <p className="hidden sm:block">افزودن به لیست علاقه مندی ها</p>
-                        </div>
-                        <div
-                            className="text-xs flex gap-x-2 items-center cursor-pointer hover:text-stone-300 transition-all duration-700">
-                            <IoIosShareAlt className="w-5 h-5"/>
-                            <p className="hidden sm:block">اشتراک گذاری</p>
-                        </div
-                        >
-                        <div className="flex gap-x-3 items-center *:cursor-pointer">
-                            <BiSolidLike className="hover:text-sky-400 transition-all duration-700"/>
-                            <BiSolidDislike className="hover:text-rose-400 transition-all duration-700"/>
-                        </div>
-                    </section>
-                    <button className="bg-white/30 p-2 rounded-md hover:bg-purple-500 transition-all duration-700">
-                        <GoKebabHorizontal className="stroke-2"/>
-                    </button>
-                </article>
+            <div className="text-xs flex gap-x-2 items-center cursor-pointer dark:text-gray-900 hover:text-stone-300 dark:hover:text-stone-500 transition-all duration-700">
+              <IoIosShareAlt className="w-5 h-5" />
+              <p className="hidden sm:block">اشتراک گذاری</p>
             </div>
-        </div>
-    );
+            <div className="flex gap-x-3 items-center *:cursor-pointer *:dark:text-gray-800">
+              <BiSolidLike className="hover:text-sky-400 transition-all duration-700" />
+              <BiSolidDislike className="hover:text-rose-400 transition-all duration-700" />
+            </div>
+          </section>
+          <button className="bg-white/30 dark:bg-black/30 p-2 dark:text-white rounded-md hover:bg-purple-500 transition-all duration-700">
+            <GoKebabHorizontal className="stroke-2" />
+          </button>
+        </article>
+      </div>
+    </div>
+  );
 }
--- src/pages/index/components/Trailers.tsx
+++ src/pages/index/components/Trailers.tsx
@@ -6,7 +6,7 @@ import Button from "../../../components/Button";
 export default function Trailers() {
     return (
         <div className="pb-10 px-5 pt-5 lg:px-0 text-center">
-            <h3 className="border-b-2 w-fit mx-auto border-white text-white pb-2 my-5">
+            <h3 className="border-b-2 w-fit mx-auto border-white dark:border-gray-900 text-white dark:text-gray-900 pb-2 my-5">
                 تریلر بازی ها
             </h3>
             <section className="w-full flex flex-col items-center gap-5">
