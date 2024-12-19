d44873075042ed2b522050208422c0f68d43fd82
saleh-moosavi <salehmoosavi77@gmail.com>
2024-12-19 10:39:51 +0330

    feat:add Register form to site

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,6 +1,93 @@
+import { useState } from "react";
+import { FaGithub, FaGoogle, FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log("Form submitted");
+  };
+
   return (
-    <div>Login</div>
-  )
+    <>
+      <div className="flex justify-center items-center min-h-[90vh] bg-cover bg-center bg-[url('https://preview.redd.it/gaming-wallpapers-3840-x-2160-uhd-v0-fr4m7cy20whd1.jpg?width=1080&crop=smart&auto=webp&s=97c6b3f7c1861af7235d329ed3d3676920d32a85')]">
+        <div className="w-80 bg-gray-800 p-8 rounded-lg text-gray-100">
+          <p className="text-center text-xl font-bold mb-4">
+            ورود به حساب کاربری
+          </p>
+
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <div className="space-y-1">
+              <label htmlFor="username" className="text-sm text-gray-400">
+                نام کاربری
+              </label>
+              <input
+                type="text"
+                id="username"
+                placeholder="نام کاربری خود را وارد کنید"
+                className="w-full text-xs px-4 py-3 bg-gray-900 border border-transparent focus:border-orange-600 rounded-md text-gray-100 focus:outline-none"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="space-y-1">
+              <label htmlFor="password" className="text-sm text-gray-400">
+                رمز عبور
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="رمر عبور خود را وارد کنید"
+                className="w-full text-xs px-4 py-3 bg-gray-900 border border-transparent focus:border-orange-600 rounded-md text-gray-100 focus:outline-none"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <div className="mt-2 block">
+                <a href="#" className="text-xs text-gray-400 hover:underline">
+                  رمز عبور خود را فراموش کرده اید ؟
+                </a>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="w-full py-3 bg-gradient-to-r from-purple-800 to-purple-600 text-gray-900 rounded-md font-semibold hover:bg-indigo-600 focus:outline-none"
+            >
+              ورود
+            </button>
+          </form>
+
+          <div className="flex items-center my-4">
+            <div className="flex-1 h-px bg-gray-600"></div>
+            <p className="px-4 text-sm text-gray-400">ورود با</p>
+            <div className="flex-1 h-px bg-gray-600"></div>
+          </div>
+
+          <div className="flex justify-center gap-x-4 items-center">
+            <button className="group p-2 rounded-md bg-transparent border border-gray-600 hover:bg-gray-700">
+              <FaGoogle className="group-hover:text-purple-500" />
+            </button>
+            <button className="group p-2 rounded-md bg-transparent border border-gray-600 hover:bg-gray-700">
+              <FaTwitter className="group-hover:text-purple-500" />
+            </button>
+            <button className="group p-2 rounded-md bg-transparent border border-gray-600 hover:bg-gray-700">
+              <FaGithub className="group-hover:text-purple-500" />
+            </button>
+          </div>
+
+          <p className="text-center text-xs text-gray-400 mt-4">
+            هنوز ثبت نام نکرده اید ؟
+            <Link
+              to={"/register"}
+              className="text-indigo-400 hover:underline mx-2"
+            >
+              ثبت نام
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { FaGithub, FaGoogle, FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Register() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
@@ -14,19 +16,17 @@ export default function Register() {
     <>
       <div className="flex justify-center items-center min-h-[90vh] bg-cover bg-center bg-[url('https://preview.redd.it/gaming-wallpapers-3840-x-2160-uhd-v0-fr4m7cy20whd1.jpg?width=1080&crop=smart&auto=webp&s=97c6b3f7c1861af7235d329ed3d3676920d32a85')]">
         <div className="w-80 bg-gray-800 p-8 rounded-lg text-gray-100">
-          <p className="text-center text-xl font-bold mb-4">
-            ورود به حساب کاربری
-          </p>
+          <p className="text-center text-xl font-bold mb-4">ثبت نام در سایت</p>
 
           <form className="space-y-4" onSubmit={handleSubmit}>
             <div className="space-y-1">
               <label htmlFor="username" className="text-sm text-gray-400">
-                نام کاربری
+                ایمیل
               </label>
               <input
-                type="text"
+                type="email"
                 id="username"
-                placeholder="نام کاربری خود را وارد کنید"
+                placeholder="ایمیل خود را وارد کنید"
                 className="w-full text-xs px-4 py-3 bg-gray-900 border border-transparent focus:border-orange-600 rounded-md text-gray-100 focus:outline-none"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
@@ -39,28 +39,36 @@ export default function Register() {
               <input
                 type="password"
                 id="password"
-                placeholder="رمر عبور خود را وارد کنید"
+                placeholder="رمر عبور مورد نظر را وارد کنید"
                 className="w-full text-xs px-4 py-3 bg-gray-900 border border-transparent focus:border-orange-600 rounded-md text-gray-100 focus:outline-none"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
-              <div className="mt-2 block">
-                <a href="#" className="text-xs text-gray-400 hover:underline">
-                  رمز عبور خود را فراموش کرده اید ؟
-                </a>
-              </div>
+            </div>
+            <div className="space-y-1">
+              <label htmlFor="password" className="text-sm text-gray-400">
+                تکرار رمز عبور
+              </label>
+              <input
+                type="password"
+                id="repassword"
+                placeholder="رمر عبور را مجدد وارد کنید"
+                className="w-full text-xs px-4 py-3 bg-gray-900 border border-transparent focus:border-orange-600 rounded-md text-gray-100 focus:outline-none"
+                value={rePassword}
+                onChange={(e) => setRePassword(e.target.value)}
+              />
             </div>
             <button
               type="submit"
               className="w-full py-3 bg-gradient-to-r from-purple-800 to-purple-600 text-gray-900 rounded-md font-semibold hover:bg-indigo-600 focus:outline-none"
             >
-              ورود
+              ثبت نام
             </button>
           </form>
 
           <div className="flex items-center my-4">
             <div className="flex-1 h-px bg-gray-600"></div>
-            <p className="px-4 text-sm text-gray-400">ورود با</p>
+            <p className="px-4 text-sm text-gray-400">ثبت نام با </p>
             <div className="flex-1 h-px bg-gray-600"></div>
           </div>
 
@@ -77,10 +85,13 @@ export default function Register() {
           </div>
 
           <p className="text-center text-xs text-gray-400 mt-4">
-            هنوز ثبت نام نکرده اید ؟
-            <a href="#" className="text-indigo-400 hover:underline mx-2">
-              ثبت نام
-            </a>
+            ثبت نام کرده اید ؟
+            <Link
+              to={"/login"}
+              className="text-indigo-400 hover:underline mx-2"
+            >
+              ورود
+            </Link>
           </p>
         </div>
       </div>
